06501add83c2f77ce81334e1e2122dca914bffad
Tyler Long <tyler4long@gmail.com>
2021-07-09 15:55:00 -0700

    First working version

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,15 @@
-console.log('Hello world!');
+import cronParser from 'cron-parser';
+import moment from 'moment-timezone';
+
+const interval = cronParser.parseExpression('1 1 1 1 1 *');
+
+const dateString = interval.prev().toISOString();
+
+const prevTimestamp = moment.tz(dateString, 'utc');
+
+const currentTimestamp = moment
+  .tz(dateString, 'utc')
+  .seconds(0)
+  .milliseconds(0);
+
+console.log((currentTimestamp as any) - (prevTimestamp as any));
